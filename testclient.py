b3a13dcb99b58507796bbcb0287074e46b7c662f
Mark Paschal <markpasc@markpasc.org>
2010-05-19 13:42:59 -0700

    Try harder to load the host-meta JRD

--- testclient.py
+++ testclient.py
@@ -1,7 +1,10 @@
+import errno
 from functools import partial
+import json
 import logging
 from os.path import join, dirname
-from urlparse import urlsplit, SplitResult
+import socket
+from urlparse import urlsplit, urlunsplit
 
 from httplib2 import Http
 from itty import *
@@ -9,6 +12,8 @@ from itty import *
 from sessionstore import SessionStore
 from templateresponse import TemplateResponse
 
+
+log = logging.getLogger()
 
 get('/static/(?P<filename>.+)')(partial(serve_static_file, root=join(dirname(__file__), 'static')))
 
@@ -25,15 +30,19 @@ def index(request):
 
 def identifier_for_url(openid_url):
     scheme, domain, path, query, fragment = urlsplit(openid_url)
-    if scheme is None:
+    log.debug("From %r, split into (%r, %r, %r)", openid_url, scheme, domain, path)
+    if not scheme:
         if '@' in path:
+            log.debug("That's an email, so pretend it's an acct: URI")
             scheme = 'acct'
         else:
             # Re-split so we get the domain in the netloc.
             scheme, domain, path, query, fragment = urlsplit('http://%s' % openid_url)
+            log.debug("Resplit the ID as 'http://%s', so now it's (%r, %r, %r)", openid_url, scheme, domain, path)
     if scheme.startswith('http') and not path:
         path = '/'
     identifier = urlunsplit((scheme, domain, path, query, fragment))
+    log.debug("Recomposed the identifier into %r", identifier)
 
     if scheme == 'acct':
         domain = '@'.split(path, 1)[-1]
@@ -41,20 +50,50 @@ def identifier_for_url(openid_url):
     return domain, identifier
 
 
+class BadResponse(Exception):
+    pass
+
+
+def hostmeta_for_domain(domain):
+    # Try to get the host-meta document.
+    http = Http()
+    try:
+        response, content = http.request('https://%s/.well-known/host-meta?format=json' % domain)
+    except socket.error, exc:
+        if exc.errno != errno.ECONNREFUSED:
+            raise
+        try:
+            response, content = http.request('http://%s/.well-known/host-meta?format=json' % domain)
+        except socket.error, exc:
+            if exc.errno == errno.ECONNREFUSED:
+                raise BadResponse('Connection refused')
+            raise
+
+    if response.status != 200:
+        raise BadResponse('%d %s' % (response.status, response.reason))
+
+    try:
+        hostmeta = json.loads(content)
+    except Exception, exc:
+        raise BadResponse('%s: %s' % (type(exc).__name__, str(exc)))
+
+    return hostmeta
+
+
 @post('/discover')
 @sessionize
 def discover(request):
-    log = logging.getLogger('discover')
     openid_url = request.POST.get('openid_url')
 
     domain, identifier = identifier_for_url(openid_url)
+    log.debug("Yay, cleaved the openid_url into %r and %r", domain, identifier)
 
-    # Try to get the host-meta document.
-    http = Http()
-    response, content = http.request('https://%s/.well-known/host-meta?format=json' % domain)
-    log.debug('From https: host-meta request, got a %d response', response.status)
+    try:
+        hostmeta = hostmeta_for_domain(domain)
+    except BadResponse, exc:
+        return Response('Oops: %s' % str(exc), content_type='text/plain')
 
-    return Response(content, content_type='text/plain')
+    return Response(json.dumps(hostmeta), content_type='text/plain')
 
 
 if __name__ == '__main__':
